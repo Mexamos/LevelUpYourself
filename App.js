02acb27973615f28ad28a9e44d8a62250fd5c9f7
m.tolstikov <m.tolstikov@brandquad.ru>
2022-02-05 02:13:35 +0300

    Sex react navigation with realm

--- App.js
+++ App.js
@@ -24,51 +24,10 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-// const Stack = createNativeStackNavigator();
-
-// const BaseText = ({ children }) => {
-//     return (
-//         <Text style={styles.baseText}>
-//             {children}
-//         </Text>
-//     )
-// }
-
-// const HomeScreen = ({ navigation }) => {
-//     return (
-//         <Button
-//             title="Go to Jane's profile"
-//             onPress={() =>
-//                 navigation.navigate('Profile', { name: 'Jane' })
-//             }
-//         />
-//     );
-// };
-// const ProfileScreen = ({ navigation, route }) => {
-//     return (
-//         <BaseText>
-//             <Text>This is {route.params.name}'s profile</Text>
-//         </BaseText>
-//     );
-// };
-
-// function LogoTitle() {
-//     return (
-//         <TouchableOpacity
-//         onPress={() => alert('This is a button!')}
-//         >
-//             <Image
-//             style={{ width: 25, height: 25 }}
-//             source={require('./assets/menu.png')}
-//             />
-//         </TouchableOpacity>
-//     );
-// }
-
+import Realm from "realm";
 
 
 
@@ -97,34 +56,12 @@ const Drawer = createDrawerNavigator();
 
 const App: () => Node = () => {
     return (
-        
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="Home">
                 <Drawer.Screen name="Home" component={HomeScreen} />
                 <Drawer.Screen name="Notifications" component={NotificationsScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
-        
-
-        // <NavigationContainer>
-        //     <Stack.Navigator>
-        //         <Stack.Screen
-        //         name="Home"
-        //         component={HomeScreen}
-        //         options={{
-        //             headerTitle: props => <LogoTitle {...props} />,
-        //             headerRight: () => (
-        //                 <Button
-        //                 onPress={() => alert('This is a button!')}
-        //                 title="Info"
-        //                 color="grey"
-        //                 />
-        //             ),
-        //         }}
-        //         />
-        //         <Stack.Screen name="Profile" component={ProfileScreen} />
-        //     </Stack.Navigator>
-        // </NavigationContainer>
     );
 };
 
